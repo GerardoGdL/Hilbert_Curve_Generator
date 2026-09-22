97b86e63f0eb0345e5fb2f3e4af59912a8946ce0
GerardoGdL <gerardo.garciadeleon@ucalgary.ca>
2026-09-22 13:38:10 -0600

    Fixed the y-scale so it behaves like regular cartesian plane

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,17 @@ int main()
 
 	sf::RenderWindow window( sf::VideoMode( { 1024, 1024 } ), "HW1 - Hilbert-Curve" );
 
+	//SFML starts the coordinate system at top left, so we switch to bottom left
+	sf::View view = window.getView();
+
+	//Puts the origin at the bottom-left by setting the center at half our window height/width
+	view.setCenter({512.f, 512.f});
+
+	//Flips the Y axis so its pointing upwards and not down
+	view.setSize({1024.f, -1024.f});
+
+	window.setView(view);
+
 	while ( window.isOpen() )
 	{
 		while ( const std::optional event = window.pollEvent() )
